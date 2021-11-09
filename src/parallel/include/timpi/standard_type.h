85fbe28a03de3c9b8408ba6487ec8f6e1b81bd63
Alex Lindsay <alexlindsay239@gmail.com>
2021-11-09 15:03:08 -0800

    Implement copy assignment operators for StandardType

--- src/parallel/include/timpi/standard_type.h
+++ src/parallel/include/timpi/standard_type.h
@@ -195,6 +195,14 @@ TIMPI_STANDARD_TYPE(long double,MPI_LONG_DOUBLE);
       timpi_call_mpi (MPI_Type_dup (t._datatype, &_datatype));
     }
 
+  StandardType & operator=(StandardType & t)
+    {
+      this->free();
+      timpi_call_mpi
+        (MPI_Type_dup (t._datatype, &_datatype));
+      return *this;
+    }
+
     ~StandardType() {
       this->free();
     }
@@ -276,6 +284,14 @@ public:
       (MPI_Type_dup (t._datatype, &_datatype));
   }
 
+  StandardType & operator=(StandardType & t)
+    {
+      this->free();
+      timpi_call_mpi
+        (MPI_Type_dup (t._datatype, &_datatype));
+      return *this;
+    }
+
   ~StandardType() { this->free(); }
 
   static const bool is_fixed_type = true;
@@ -350,6 +366,14 @@ public:
       (MPI_Type_dup (t._datatype, &_datatype));
   }
 
+  StandardType & operator=(StandardType & t)
+    {
+      this->free();
+      timpi_call_mpi
+        (MPI_Type_dup (t._datatype, &_datatype));
+      return *this;
+    }
+
   ~StandardType() { this->free(); }
 
   static const bool is_fixed_type = true;
@@ -386,7 +410,7 @@ void BuildStandardTypeVector<n_minus_i>::build
     ith_type;
 
   out_vec.emplace_back
-    (new StandardType<ith_type>
+    (std::make_unique<StandardType<ith_type>>
      (&std::get<sizeof...(Types)-n_minus_i>(example)));
 
   BuildStandardTypeVector<n_minus_i-1>::build(out_vec, example);
@@ -505,6 +529,14 @@ public:
     timpi_call_mpi
       (MPI_Type_dup (t._datatype, &_datatype));
   }
+
+  StandardType & operator=(StandardType & t)
+    {
+      this->free();
+      timpi_call_mpi
+        (MPI_Type_dup (t._datatype, &_datatype));
+      return *this;
+    }
 
   ~StandardType() { this->free(); }
 
